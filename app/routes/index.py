e5430e526d389325ae7a6335a38172c62f639a4b
petersonmardi <petersonmardi@gmail.com>
2023-05-29 19:37:19 -0400

    Add the function login_required dependency

--- app/routes/index.py
+++ app/routes/index.py
@@ -7,10 +7,12 @@ from flask import (
 from ..models.inventory_model import InventoryModel
 
 from ..extensions import db
+from ..extensions import login_required
 
 index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
+@login_required
 def index():
     columns = InventoryModel.query.all()
     return render_template('index.html', columns=columns)
