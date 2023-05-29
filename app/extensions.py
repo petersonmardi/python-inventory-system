bdd738e308dbe1784a61f6f518c7050b1c7d51e5
petersonmardi <petersonmardi@gmail.com>
2023-05-29 19:31:54 -0400

    Add Flask-Login extension

--- app/extensions.py
+++ app/extensions.py
@@ -5,3 +5,18 @@ db = SQLAlchemy()
 from flask_migrate import Migrate
 
 migrate = Migrate()
+
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+
+UserMixin = UserMixin
+
+login_user = login_user
+
+login_manager = LoginManager()
+
+login_required = login_required
+
+logout_user = logout_user
+
+current_user = current_user
+
